975936bbd3802faede09d2241dda270e5a0781a5
Nayara <nayaramaisa.stn@gmail.com>
2024-11-07 10:03:29 -0300

    edicao final frontend

--- main.js
+++ main.js
@@ -8,7 +8,18 @@ $(document).ready(function () {
     })
 
     $('form').on('submit', function (e) {
-        console.log("submit")
         e.preventDefault()
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display:none"></li>')
+        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem) //criado no jquery o que vai puxar o imagem nova adicionada, o appendeTo pega o elemento da frente e insere no li de cima
+        $(`
+            <div class="overlay-imagem-link">
+            <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamnho real">  
+                Ver imagem em tamanho real
+            </a>
+            </div>`).appendTo(novoItem) //target blank faz com que a imagem se abra em outra pagina
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1000) //faz com que a imagem surja com efeito, pois no li a cima colocou display none, entao ao adicionar ele fica oculto a principio. O 1000 é 1 segundo
+        $('#endereco-imagem-nova').val('')//limpar o campo(formulario) apos coloar a foto
     })
 })
